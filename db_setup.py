da40effbaf4b41190fc4ca2fe0083c0f3f484731
Zach Loomas <zloomas@gmail.com>
2022-03-08 01:23:49 -0500

    create db_setup.py

---
+++ db_setup.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+conn = sqlite3.connect('lt_osf.db')
+
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS users
+           (id TEXT PRIMARY KEY,
+           full_name TEXT,
+           date_created TEXT);
+    """
+)
+
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS jobs
+           (id TEXT,
+           title TEXT,
+           institution TEXT,
+           start_year INT,
+           end_year INT,
+           ongoing INT);
+    """
+)
+
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS education
+           (id TEXT,
+           degree TEXT,
+           institution TEXT,
+           start_year INT,
+           end_year INT,
+           ongoing INT);
+    """
+)
+
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS socials
+           (id TEXT,
+           platform TEXT,
+           name TEXT);
+    """
+)
+
+conn.commit()
+conn.close()
+

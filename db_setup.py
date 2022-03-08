d1cf5c3cee43b6202c8b05882680b212c6e78670
Zach Loomas <zloomas@gmail.com>
2022-03-08 15:53:07 -0500

    improve specification of existing tables and add more

--- db_setup.py
+++ db_setup.py
@@ -4,43 +4,90 @@ conn = sqlite3.connect('lt_osf.sqlite')
 
 conn.execute(
     """
-    CREATE TABLE IF NOT EXISTS users
-           (id TEXT PRIMARY KEY,
-           full_name TEXT,
-           date_created TEXT);
+    CREATE TABLE IF NOT EXISTS users(
+           id TEXT PRIMARY KEY,
+           full_name TEXT NOT NULL,
+           date_created TEXT NOT NULL
+           );
     """
 )
 
 conn.execute(
     """
-    CREATE TABLE IF NOT EXISTS jobs
-           (id TEXT,
-           title TEXT,
-           institution TEXT,
+    CREATE TABLE IF NOT EXISTS jobs(
+           id TEXT NOT NULL,
+           title TEXT NOT NULL,
+           institution TEXT NOT NULL,
            start_year INT,
            end_year INT,
-           ongoing INT);
+           ongoing INT NOT NULL,
+           UNIQUE(id, title, institution)
+           );
     """
 )
 
 conn.execute(
     """
-    CREATE TABLE IF NOT EXISTS education
-           (id TEXT,
-           degree TEXT,
-           institution TEXT,
+    CREATE TABLE IF NOT EXISTS education(
+           id TEXT NOT NULL,
+           degree TEXT NOT NULL,
+           institution TEXT NOT NULL,
            start_year INT,
            end_year INT,
-           ongoing INT);
+           ongoing INT,
+           UNIQUE(id, degree, institution)
+           );
     """
 )
 
 conn.execute(
     """
-    CREATE TABLE IF NOT EXISTS socials
-           (id TEXT,
+    CREATE TABLE IF NOT EXISTS socials(
+           id TEXT,
            platform TEXT,
-           name TEXT);
+           name TEXT,
+           UNIQUE(id, platform, name)
+           );
+    """
+)
+
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS nodes(
+           id TEXT PRIMARY KEY,
+           title TEXT,
+           date_created TEXT
+           );
+    """
+)
+
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS node_tags(
+           id TEXT,
+           tag TEXT,
+           UNIQUE(id, tag)
+           );
+    """
+)
+
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS node_children(
+           parent TEXT,
+           child TEXT,
+           UNIQUE(parent, child)
+           );
+    """
+)
+
+conn.execute(
+    """
+    CREATE TABLE IF NOT EXISTS contributors(
+           user TEXT,
+           node TEXT,
+           UNIQUE(user, node)
+           );
     """
 )
 

894c2b3c50f3c97de1c70d8616800dd2619f371e
Zach Loomas <47288744+zloomas@users.noreply.github.com>
2022-03-15 03:28:05 -0400

    restrict edges export to mixed internal/external pairs only

--- network_functions.py
+++ network_functions.py
@@ -42,6 +42,7 @@ def create_network():
         """
         SELECT u.id,
                u.full_name,
+               u.date_created,
                cos.current
           FROM users u
           LEFT JOIN cos_staff cos ON u.id=cos.id
@@ -52,7 +53,8 @@ def create_network():
 
     conn.close()
 
-    users_df = pd.DataFrame(users, columns=['guid', 'full_name', 'is_cos'])
+    users_df = pd.DataFrame(users, columns=['guid', 'full_name', 'date_created', 'is_cos'])
+    cos_staff = users_df[users_df.is_cos.notna()].guid.values
 
     contributors_ = {n[0]: [] for n in nodes}
 
@@ -63,34 +65,13 @@ def create_network():
 
     for node, users in contributors_.items():
         for edge in itertools.combinations(users, 2):
-            edges.append((edge[0], edge[1], node))
+            a = edge[0]
+            b = edge[1]
+            if a in cos_staff and b not in cos_staff:
+                edges.append((a, b, node))
+            elif a not in cos_staff and b in cos_staff:
+                edges.append((b, a, node))
 
-    edges_df = pd.DataFrame(edges, columns=['user_a', 'user_b', 'project_guid'])
+    edges_df = pd.DataFrame(edges, columns=['internal', 'external', 'project_guid'])
 
     return users_df, edges_df
-
-
-def assign_edge_type(row, ref):
-    """
-    Determines whether given edge (provided as row in dataframe with at least two columns of users, user_a and user_b)
-    is composed of two internal nodes, one internal and one external node, or two external nodes. Requires reference
-    list of internal nodes. Intended to be used with pandas.DataFrame.apply.
-
-    :param row: row in dataframe with at least two columns of users, user_a and user_b
-    :param ref: reference list of internal nodes
-    :return: str, edge type, one of 'internal', 'mixed_a' (user_a is internal), 'mixed_b' (user_a is internal), 'external'
-    """
-    a = row['user_a'] in ref
-    b = row['user_b'] in ref
-
-    if a and b:
-        edge_type = 'internal'
-    elif a and not b:
-        edge_type = 'mixed_a'
-    elif not a and b:
-        edge_type = 'mixed_b'
-    else:
-        edge_type = 'external'
-
-    return edge_type
-
